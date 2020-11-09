c8d7ced7c87c3f98f925555a7e9939a5af912854
t3qquq <63445961+t3qquq@users.noreply.github.com>
2020-11-10 00:06:57 +0900

    job title, job location 完

--- main.py
+++ main.py
@@ -9,15 +9,26 @@ indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
 
 indeed_page_number = len(indeed_soup.find('div', {'class':'pagination'}).find_all('span', {'class':'pn'}))
 
-job_titles=[]
+job_info_list = []
 
 for page in range(indeed_page_number):
     indeed_page_soup =BeautifulSoup(requests.get(f'https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start={page*50}').text, 'html.parser')
-
     job_info_html = indeed_page_soup.find_all('div', {'class':'jobsearch-SerpJobCard'})
 
     for job_num in range(len(job_info_html)):
-        job_titles.append(job_info_html[job_num].find('a').get('title'))
+        job_title = job_info_html[job_num].find('a').get('title')
+        job_company = job_info_html[job_num].find('span',{'class' : 'company'}).string
+        if(job_info_html[job_num].find('span',{'class' : 'location'}) is not None):
+            job_location = job_info_html[job_num].find('span',{'class' : 'location'}).string
+        else:
+            job_location = job_info_html[job_num].find('div',{'class':'location'}).string
+        job_info = {'title':job_title , 'company':job_company, 'location':job_location}
+        job_info_list.append(job_info)
 
-for num in range(len(job_titles)):
-    print(job_titles[num])
+for num in range(len(job_info_list)):
+    print(job_info_list[num]) 
+
+
+# indeed_page_soup =BeautifulSoup(requests.get(f'https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=0').text, 'html.parser')
+# job_info_html = indeed_page_soup.find_all('div', {'class':'jobsearch-SerpJobCard'})
+# print(job_info_html[0].find('span',{'class' : 'location'}).string)
