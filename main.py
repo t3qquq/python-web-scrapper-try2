c08a67805d2500a998e4e90fdc5e7811532f2a47
t3qquq <63445961+t3qquq@users.noreply.github.com>
2020-11-10 09:52:29 +0900

    main에서 indeed.py 분리

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import indeed_job_extract
 
-START = 0
-INDEED_URL = f'https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start={START}'
-
-indeed_result = requests.get(INDEED_URL)
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-
-indeed_page_number = len(indeed_soup.find('div', {'class':'pagination'}).find_all('span', {'class':'pn'}))
-
-job_info_list = []
-
-for page in range(indeed_page_number):
-    indeed_page_soup =BeautifulSoup(requests.get(f'https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start={page*50}').text, 'html.parser')
-    job_info_html = indeed_page_soup.find_all('div', {'class':'jobsearch-SerpJobCard'})
-
-    for job_num in range(len(job_info_html)):
-        job_title = job_info_html[job_num].find('a').get('title')
-        if(job_info_html[job_num].find('span',{'class' : 'company'}).string is not None):
-            job_company = job_info_html[job_num].find('span',{'class' : 'company'}).string
-        else:
-            job_company = job_info_html[job_num].find('span',{'class' : 'company'}).find('a').get_text()
-        job_company = job_company.replace('\n','')
-        if(job_info_html[job_num].find('span',{'class' : 'location'}) is not None):
-            job_location = job_info_html[job_num].find('span',{'class' : 'location'}).string
-        else:
-            job_location = job_info_html[job_num].find('div',{'class':'location'}).string
-        job_info = {'title':job_title , 'company':job_company, 'location':job_location}
-        job_info_list.append(job_info)
-
-for num in range(len(job_info_list)):
-    print(job_info_list[num]) 
-
-
-# indeed_page_soup =BeautifulSoup(requests.get(f'https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=0').text, 'html.parser')
-# job_info_html = indeed_page_soup.find_all('div', {'class':'jobsearch-SerpJobCard'})
-# print(job_info_html[0].find('span',{'class' : 'location'}).string)
+indeed_jobs = indeed_job_extract()
+for num in range(len(indeed_jobs)):
+    print(indeed_jobs[num])
