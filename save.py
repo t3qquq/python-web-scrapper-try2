24576815b10210a757d5f5c7b95e770f91e9c04f
shin-jaewon <shine970530@gmail.com>
2020-11-26 10:57:01 +0900

    nothing

--- save.py
+++ save.py
@@ -1,9 +1,10 @@
 import csv
 
+
 def save_to_file(jobs):
     file = open('jobs.csv', mode='w')
     writer = csv.writer(file)
+
     writer.writerow(['title', 'company', 'location'])
-    
-    for num in range(len(jobs)):
-        writer.writerow([jobs[num]['title'], jobs[num]['company'], jobs[num]['location']])
+    for job in jobs:
+        writer.writerow(list(job.values()))
